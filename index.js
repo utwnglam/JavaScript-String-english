c9bf9c7f2100eaa260d3074c4c082d0a65a56754
JOANNE CHEUNG (PDM-CS/HKG) <CHEUNJO2@OOCL.COM>
2021-11-18 11:01:14 +0800

    complete tasks in index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
+console.log(' \'' + name.toUpperCase() + '\' ');
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+const arr = sentence.split(" ");
+for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i][0].toUpperCase() + arr[i].substring(1);
+}
+console.log(arr.join(" "));
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+let value = parseInt(money.substring(1));
+console.log(value);
